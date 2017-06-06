1fe68fe15e12f16426119c2c9dc18dc89edea175
Vladimir Livin <vladimir@livesystems.ru>
2017-06-06 20:38:33 +0300

    Test: Ensure that receipt's transaction price can be used as single item price
    The test added to ensure that receipt.transaction.price can be used with New API Endpoints to get the price of a single item in the order. Test checks both order created after releasing new API and order created with old API using variations.

---
+++ Etsy_API_Java_Library/test/com/etsy/ReceiptRequestTest.java
@@ -0,0 +1,72 @@
+package com.etsy;
+
+import com.etsy.etsyCore.EtsyResult;
+import com.etsy.etsyModels.Receipt;
+import com.etsy.etsyModels.Transaction;
+import com.etsy.etsyRequests.ReceiptsRequest;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import software.betamax.Configuration;
+import software.betamax.junit.Betamax;
+import software.betamax.junit.RecorderRule;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+/**
+ * @author Vladimir Livin, vladimir@livesystems.ru
+ */
+public class ReceiptRequestTest extends EtsyRequestTest {
+    @Before
+    public void setUp() {
+        super.setUp();
+    }
+
+    @Rule
+    public RecorderRule recorder = new RecorderRule(Configuration.builder().sslEnabled(true).build());
+
+    @Betamax(tape="NewInventoryAPITape")
+    @Test
+    public void receiptWithDifferentOfferingsShouldHaveCorrectTransactionPrice() {
+        ReceiptsRequest request = ReceiptsRequest.getReceipt("1197623530");
+        Map<String, String> params = new HashMap<>();
+        params.put("includes", "Listings,Transactions,Transactions/Listing,Buyer/Addresses,Buyer/Country,Order,Coupon");
+        request.addParams(params);
+        EtsyResult result = requestManager.runRequest(request);
+
+        assertNull(result.getError());
+        assertEquals(200, result.getCode());
+        assertEquals(1, result.getResults().size());
+        Receipt receipt = (Receipt) result.getResults().get(0);
+        assertEquals("Receipt should have 1 transaction", 1, receipt.getTransactions().length);
+        Transaction transaction = receipt.getTransactions()[0];
+        assertEquals("Price should be 1 USD and represent single item price",1.0, transaction.getPrice(), 0.00001);
+        assertEquals("Currency should be USD","USD", transaction.getCurrencyCode());
+        assertEquals(1, transaction.getQuantity());
+    }
+
+    @Betamax(tape="NewInventoryAPITape")
+    @Test
+    public void receiptCreatedWithOldVariationsShouldHaveCorrectTransactionPrice() {
+        ReceiptsRequest request = ReceiptsRequest.getReceipt("98232671");
+        Map<String, String> params = new HashMap<>();
+        params.put("includes", "Listings,Transactions,Transactions/Listing,Buyer/Addresses,Buyer/Country,Order,Coupon");
+        request.addParams(params);
+        EtsyResult result = requestManager.runRequest(request);
+
+        assertNull(result.getError());
+        assertEquals(200, result.getCode());
+        assertEquals(1, result.getResults().size());
+        Receipt receipt = (Receipt) result.getResults().get(0);
+        assertEquals("Receipt should have 1 transaction", 1, receipt.getTransactions().length);
+        Transaction transaction = receipt.getTransactions()[0];
+        assertEquals("Price should be 1 USD and represent single item price",1.0, transaction.getPrice(), 0.00001);
+        assertEquals("Currency should be USD","USD", transaction.getCurrencyCode());
+        assertEquals(3, transaction.getQuantity());
+    }
+
+}
